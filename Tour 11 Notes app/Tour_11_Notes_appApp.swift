5aaa0d001b734688f673c9d28d660eabedb94346
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2022-11-05 07:40:21 +0300

    Initial Commit

---
+++ Tour 11 Notes app/Tour_11_Notes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tour_11_Notes_appApp.swift
+//  Tour 11 Notes app
+//
+//  Created by Collins Muthomi on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tour_11_Notes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
